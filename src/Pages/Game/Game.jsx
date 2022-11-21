02411a45bfb1c350dea894dbff197b5c1d2306d6
Flavio Silva <55815265+201flaviosilva@users.noreply.github.com>
2022-11-21 18:53:50 +0000

    Update Game.jsx

--- src/Pages/Game/Game.jsx
+++ src/Pages/Game/Game.jsx
@@ -44,7 +44,7 @@ function ActionsPanel({ setModal }) {
 				<GiSettingsKnobs size={ICON_SIZE} />
 			</ButtonIcon>
 			<ButtonIcon>
-				<a href="https://github.com/201flaviosilva/arbor" target="_blank">
+				<a href="https://github.com/201flaviosilva/arbor" target="_blank" rel="noreferrer">
 					<AiFillGithub size={ICON_SIZE} />
 				</a>
 			</ButtonIcon>
